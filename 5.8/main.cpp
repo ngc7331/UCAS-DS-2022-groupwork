d1744bcdc7725f3d8d51f527465936176514c4f6
ngc7331 <ngc7331@outlook.com>
2022-06-26 11:50:11 +0800

    feat(5.8): help()

--- 5.8/main.cpp
+++ 5.8/main.cpp
@@ -5,6 +5,7 @@
 #include "type.h"
 
 void help();
+std::string EXE_NAME;
 
 // use "extern" in other files
 ALGO_TYPE algorithm;
@@ -14,6 +15,7 @@ std::map<int, std::string> plane_list;
 
 int main(int argc, char **argv) {
     // init data
+    EXE_NAME = *argv;
     enum {TERMINAL, SERVER} intr_mode = TERMINAL;
     Server server;
     algorithm = ALGO_DP;
@@ -26,24 +28,29 @@ int main(int argc, char **argv) {
     }
 
     // arg parse
-    while (argc --) {
+    while (++argv, --argc) {
         std::string arg = *argv;
         if (arg == "-p" || arg == "--port") {
             argc --;
             argv ++;
             server.setPort(atoi(*argv));
-        };
-        if (arg == "-t" || arg == "--terminal") intr_mode = TERMINAL;
-        if (arg == "-s" || arg == "--server") intr_mode = SERVER;
-        if (arg == "-h" || arg == "--help") {
+        }
+        else if (arg == "-t" || arg == "--terminal") intr_mode = TERMINAL;
+        else if (arg == "-s" || arg == "--server") intr_mode = SERVER;
+        else if (arg == "-h" || arg == "--help") {
             help();
             exit(0);
-        };
-        argv ++;
+        }
+        else {
+            std::cout << "参数无效: "<< arg << std::endl;
+            help();
+            exit(1);
+        }
     }
 
     switch (intr_mode) {
     case TERMINAL:
+        std::cout << "你启用了尚未完成的终端模式orz" << std::endl;
         break;
     case SERVER:
         server.run();
@@ -54,5 +61,11 @@ int main(int argc, char **argv) {
 }
 
 void help() {
-    // TODO: print help message
+    std::cout << "usage: '" << EXE_NAME << " [--terminal] [--server [--port] <port>] [--help]'" << std::endl;
+    std::cout << "  --terminal 或无参数的情况下启用终端模式" << std::endl;
+    std::cout << "  --server   启用服务器 + webui 模式，默认情况下使用8080端口" << std::endl;
+    std::cout << "  --port     指定服务器端口，紧跟<port>为端口号，仅在使用--server时有效" << std::endl;
+    std::cout << "  --help     显示帮助信息并退出" << std::endl;
+    std::cout << "如参数有冲突，例如同时使用--server和--terminal，将以最后的为准" << std::endl;
+    std::cout << "支持使用首字母缩写，例如-s等价于--server" << std::endl;
 }
