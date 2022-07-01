191803485f3ecdb2acf3f67d06ea6ba4df8b1ff8
Muzi <lyj_muzi@qq.com>
2022-07-01 18:22:36 +0800

    complete the overall structure of main

--- 6.7/main.cpp
+++ 6.7/main.cpp
@@ -5,13 +5,32 @@ using namespace std;
 int len;
 Record record[SIZE];
 
+int priority[5];
+
 int main()
 {
+    int tp;
+    scanf("%d", &tp);
     scanf("%d", &valNumber);
     for (int i = 0; i < valNumber; ++ i)
         scanf("%d", &priority[i]), -- priority[i];
     scanf("%d", &len);
     for (int i = 0; i < len; ++ i)
+    {
+        int tmp[5];
         for (int j = 0; j < valNumber; ++ j)
-            scanf("%d", &record[i].val[j]);
+            scanf("%d", &tmp[j]);
+        for (int j = 0; j < valNumber; ++ j)
+            record[i].val[j] = tmp[priority[j]];
+    }
+    LSD :: internalSort(record, len);
+    for (int i = 0; i < len; ++ i)
+    {
+        int tmp[5];
+        for (int j = 0; j < valNumber; ++ j)
+            printf("%d ", tmp[priority[j]] = record[i].val[j]);
+        for (int j = 0; j < valNumber; ++ j)
+            printf("%d ", tmp[j]);
+        printf("\n");
+    }
 }
