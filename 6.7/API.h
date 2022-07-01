de34cd1546f99199284be8c43ab93f7220395f2a
Muzi <lyj_muzi@qq.com>
2022-07-01 18:15:11 +0800

    delete operator< and operator>

--- 6.7/API.h
+++ 6.7/API.h
@@ -7,28 +7,8 @@ int valNumber; //number of values in a record
 
 struct Record
 {
-    int val[5];
+    int val[5]; //ordered by priority
 };
-
-bool operator<(Record a, Record b)
-{
-    for (int i = 0; i < valNumber - 2; ++ i)
-    {
-        if (a.val[priority[i]] != b.val[priority[i]])
-            return a.val[priority[i]] < b.val[priority[i]];
-    }
-    return a.val[priority[valNumber - 1]] < b.val[priority[valNumber - 1]];
-}
-
-bool operator>(Record a, Record b)
-{
-    for (int i = 0; i < valNumber - 2; ++ i)
-    {
-        if (a.val[priority[i]] != b.val[priority[i]])
-            return a.val[priority[i]] > b.val[priority[i]];
-    }
-    return a.val[priority[valNumber - 1]] > b.val[priority[valNumber - 1]];
-}
 
 namespace LSD
 {
