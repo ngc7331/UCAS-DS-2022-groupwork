8e04ad351f4cc42f86f70c161dcbd191c9adde42
Eureka Wang <eurekaw@163.com>
2022-07-07 20:23:08 +0800

    fix(6.7): add const before args

--- 6.7/MSD.cpp
+++ 6.7/MSD.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 
 extern bool DEBUGMODE;
+
 #define DEBUG(expr) \
     if (DEBUGMODE)  \
     {               \
@@ -13,14 +14,17 @@ extern bool DEBUGMODE;
 //在依据key0, key1两个关键字时，使用非递归的归并排序，因为此时需要排序的人数比较多
 //而依据key2, key3, key4排序时，使用直接插入排序，因为此时待排序的人数比较少
 
-void Merge(Record *ARR, int Priority, int L, int M, int R)
+// int debugLen = 13;
+// Record debugArr[1000];
+
+void Merge(Record *ARR, const int Priority, const int L, const int M, const int R)
 {
     Record *arr = new Record[R - L + 1];
     int lIndex, rIndex, arrIndex;
     lIndex = L, rIndex = M + 1, arrIndex = 0;
 
     while ( lIndex <= M && rIndex <= R){
-        if (ARR[lIndex].val[Priority] > ARR[rIndex].val[Priority])
+        if (ARR[lIndex].val[Priority] < ARR[rIndex].val[Priority])
             arr[arrIndex++] = ARR[lIndex++];
         else
             arr[arrIndex++] = ARR[rIndex++];
@@ -33,22 +37,25 @@ void Merge(Record *ARR, int Priority, int L, int M, int R)
         arr[arrIndex++] = ARR[rIndex++];
     }
 
-    while(arrIndex >=0)
-        ARR[R--] = arr[arrIndex--];
+    for (int i = 0; i < arrIndex;i++)
+        ARR[L + i] = arr[i];
+
+    delete arr;
+    DEBUG(std::cout << "Merge() called with L = " << L << ", M = " << M << ", R = " << R << std::endl;)
 }
 
 // ARR: 大数组
 // Priority：依据的优先级
 // L: 最左侧的index
 // R: 最右侧的index
-void MergeSort(Record *ARR, int Priority, int L, int R)
+void MergeSort(Record *ARR, const int Priority, const int L, const int R)
 {
-    DEBUG(std::cout << "MergeSort() called with  Priority " << Priority << std::endl;)
+    DEBUG(std::cout << "MergeSort() called with Priority = " << Priority <<", L = " << L << ", R = " << R << std::endl;)
 
     int len = R - L + 1;
 
     for (int i = 1; i < len; i += i){
-        int left = 0;
+        int left = L;
         int mid = left + i - 1;
         int right = mid + i;
 
@@ -66,7 +73,7 @@ void MergeSort(Record *ARR, int Priority, int L, int R)
     }
 }
 
-void SimpleSelectSort(Record *ARR, int Priority, int L, int R)
+void SimpleSelectSort(Record *ARR, const int Priority, const int L, const int R)
 {
     DEBUG(std::cout << "SimpleSelectSort() called with  Priority " << Priority << std::endl;)
 
@@ -87,7 +94,8 @@ void SimpleSelectSort(Record *ARR, int Priority, int L, int R)
     }
 }
 
-void MSDSort(Record *ARR, int Priority, int L, int R){
+void MSDSort(Record *ARR, const int Priority, const int L, const int R)
+{
     if (Priority < 2)
         MergeSort(ARR, Priority, L, R);
     else if (Priority < PRIORITY_NUM)
@@ -99,10 +107,9 @@ void MSDSort(Record *ARR, int Priority, int L, int R){
 // desp: internal sort
 // args: Recoord record array
 // args: int length of record array
-void MSD::internalSort(Record *ARR, int ArrLen)
+void MSD::internalSort(Record *ARR, const int ArrLen)
 {
     DEBUGMODE = false;
-
     DEBUG(std::cout << "internalSort() called \n";)
 
     int priority = 0;
@@ -118,9 +125,32 @@ void MSD::internalSort(Record *ARR, int ArrLen)
                 len++;
                 i++;
             }
-            if(len!=0)
-                MSDSort(ARR, priority + 1, i - len, i - 1);
+            if (len != 0)
+                MSDSort(ARR, priority + 1, i - len - 1, i - 1);
             i++;
+            }
         }
-    }
 }
+
+// int main()
+// {
+//     debugArr[0] = {1, 2, 3, 5, 1};
+//     debugArr[1] = {1, 2, 3, 4, 2};
+//     debugArr[2] = {1, 2, 3, 3, 3};
+//     debugArr[3] = {1, 2, 3, 2, 4};
+//     debugArr[4] = {1, 2, 3, 1, 5};
+//     debugArr[5] = {2, 3, 1, 5, 1};
+//     debugArr[6] = {2, 3, 2, 4, 1};
+//     debugArr[7] = {2, 3, 3, 3, 1};
+//     debugArr[8] = {2, 3, 4, 2, 1};
+//     debugArr[9] = {2, 3, 5, 1, 1};
+//     debugArr[10] = {3, 4, 5, 1, 2};
+//     debugArr[11] = {4, 5, 1, 2, 3};
+//     debugArr[12] = {5, 1, 2, 3, 4};
+//     MSD::internalSort(debugArr, debugLen);
+//     for (int i = 0; i < debugLen; i++)
+//     {
+//         std::cout << debugArr[i].val[0] << " " << debugArr[i].val[1] << " " << debugArr[i].val[2] << " " << debugArr[i].val[3] << " " << debugArr[i].val[4] << std::endl;
+//     }
+//     return 0;
+// }
